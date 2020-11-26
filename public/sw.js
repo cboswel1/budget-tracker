f2d93178b63089f500f7bc19f2f4426a2c3fe18a
cboswel1 <christianboswell86@gmail.com>
2020-11-25 19:41:40 -0700

    fixed syntax bug

--- public/sw.js
+++ public/sw.js
@@ -3,13 +3,12 @@ const dataCacheName = "data-v1";
 
 const cacheFiles = [
   "/",
-  "/index.html",
-  "/style.css",
+  "/styles.css",
   "/db.js",
   "/index.js",
   "/manifest.webmanifest",
   "/icons/icon-192x192.png",
-  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
 ];
 
 self.addEventListener("install", event => {
